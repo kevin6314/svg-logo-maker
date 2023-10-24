cbd23b834549415d4975905b5f8922ba2519623e
limrod631 <kevin.omwega@gmail.com>
2023-10-24 09:38:49 -0500

    first draft

---
+++ index.js
@@ -0,0 +1,5 @@
+const CLI = require('./lib/cli');
+//import CLI from './lib/cli';
+const cli = new CLI();
+
+cli.run();
---
+++ test/test.js
@@ -0,0 +1,22 @@
+const {Circle, Triangle, Square} = require('../lib/shapes');
+
+describe('Shapes', () => {
+    test('should render svg code for the triangle', () => {
+        const shape = new Triangle();
+        shape.setColor("blue");
+        expect(shape.render()).toEqual(`<polygon points="150, 18 244, 182 56, 182" fill="blue" />`);
+    });
+
+    test('should render svg code for the square', () => {
+        const shape = new Square();
+        shape.setColor("blue");
+        expect(shape.render()).toEqual(`<rect width="100%" height="100%" fill="blue"/>`);
+    });
+
+    test('should render svg code for the circle', () => {
+        const shape = new Circle();
+        shape.setColor("blue");
+        expect(shape.render()).toEqual(`<circle cx="50%" cy="50%" r="100" fill="blue"/>`);
+    });
+
+})
